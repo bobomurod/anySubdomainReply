01a21c1521598364cdc36cd720b2c33faedcb386
bobomurod <bobomurod.boboev@yandex.ru>
2019-04-02 22:12:05 +0500

    :cheers:

--- responder.js
+++ responder.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('Subdomain -> ' + req.headers['x-subdomain'])
+    res.send('Subdomain -> ' + req.headers['x-subdomain'] + endl)
 });
 
 app.listen(3000, function(){
